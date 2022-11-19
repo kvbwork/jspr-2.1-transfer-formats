f8d44be7ca3342cb14057c3bd935fb332a6507be
kvbwork <kvbdev@yandex.ru>
2022-11-19 15:38:24 +0500

    Server refactor
    use ConcurrentHashMap
    less memory consumption with method/path/handler store

--- src/main/java/ru/netology/http/Server.java
+++ src/main/java/ru/netology/http/Server.java
@@ -4,8 +4,8 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -14,25 +14,25 @@ import static java.util.Optional.ofNullable;
 public class Server implements Handler {
     private static final int NUM_THREADS = 64;
 
-    private final Map<String, Map<String, Handler>> handlersPathMap;
+    private final Map<String, Map<String, Handler>> handlersMethodMap;
 
     protected final ExecutorService executor;
 
     public Server() {
-        this.handlersPathMap = new HashMap<>();
+        this.handlersMethodMap = new ConcurrentHashMap<>();
         this.executor = Executors.newFixedThreadPool(NUM_THREADS);
     }
 
     public void addHandler(String method, String path, Handler handler) {
         var methodKey = method.toUpperCase();
-        var methodMap = handlersPathMap.computeIfAbsent(path, k -> new HashMap<>());
-        methodMap.put(methodKey, handler);
+        var pathMap = handlersMethodMap.computeIfAbsent(methodKey, k -> new ConcurrentHashMap<>());
+        pathMap.put(path, handler);
     }
 
     @Override
     public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
-        ofNullable(handlersPathMap.get(request.getPath()))
-                .map(methodMap -> methodMap.get(request.getMethod()))
+        ofNullable(handlersMethodMap.get(request.getMethod()))
+                .map(pathMap -> pathMap.get(request.getPath()))
                 .orElseGet(() -> new StatusHandler(HttpStatus.NOT_FOUND))
                 .handle(request, responseStream);
     }
